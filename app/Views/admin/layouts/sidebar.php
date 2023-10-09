66517727ed6ca0e427e3a5707c971a33cf4379e4
yoni3443 <yoni@undiksha.ac.id>
2023-10-09 20:36:25 +0800

    penambahan tampilan admin

--- app/Views/admin/layouts/sidebar.php
+++ app/Views/admin/layouts/sidebar.php
@@ -4,8 +4,7 @@
     <a href="index3.html" class="brand-link">
         <img src="<?php echo base_url() ?>adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
+        <span class="brand-text font-weight-light">AdminLTE 3</span>    </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
---
+++ app/Views2/admin_super/users/index.php
@@ -0,0 +1,118 @@
+<?= $this->include('admin_super/layouts/header'); ?>
+<?= $this->include('admin_super/layouts/sidebar'); ?>
+
+<style>
+
+
+.table td{
+    text-align: center;
+    background-color: #C5E9DE;
+
+}
+
+
+.table>thead>tr>* {
+  background-color: #20c997;
+    text-align: center;
+}
+
+
+.btnadd{
+
+  padding-left: 17px;
+}
+
+.btnr{
+
+padding-inline-end: 15%;
+
+}
+
+.icon-jarak{
+  padding-right: 10px;
+}
+
+.jarak{
+  justify-content: space-between;
+}
+
+</style>
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid ">
+        <div class="row mb-4">
+          <div class="col-sm-6 px-4">
+            <h1 class="m-0 font-weight-bold " >List Users</h1>
+          </div><!-- /.col -->
+          
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- Main content -->
+    <section class="content">
+    <div class="row jarak ">
+    <div class="card-tools">
+
+    <div class="btnadd">
+    <button type="button" class="btn btn-success"><i class="icon-jarak fas fa-plus"></i>Tambah </button> <br>
+                    </div>
+</div>
+<div class="card-tools">
+<div class="input-group input-group-sm" style="width: 150px;">
+<input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+<div class="input-group-append">
+<button type="submit" class="btn btn-default">
+<i class="fas fa-search"></i>
+</button>
+</div>
+</div>
+</div>
+
+
+<div class=" card-body table-responsive p-10">
+<table class="table table-bordered table-hover text-nowrap">
+<thead>
+<tr>
+<th>No.</th>
+<th>NIP</th>
+<th>Nama</th>
+<th>Email</th>
+<th>No.Telp</th>
+<th>Dinas</th>
+<th>Bidang</th>
+<th>Aksi</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>1</td>
+<td>2013239131</td>
+<td>John Bogler</td>
+<td>Johnbog@gmail.com</td>
+<td>088282123</td>
+<td>Kominfosanti</td>
+<td>umum</td>
+<td> 
+
+<div class="btn-group ">
+                        <a class="btnr" href="#">
+                        <button type="button" class="btn btn-block btn-warning "> <i class=" fas fa-pen"></i></button>
+                        </a>
+                        <a class="btnr" href="#">
+                        <button type="button" class="btn btn-block btn-danger"><i class=" fas fa-trash"></i></button>
+                        </a>
+                    </div>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+
+</div>
+
+</div>
+
+  <?= $this->include('admin_super/layouts/footer'); ?>
+ 
