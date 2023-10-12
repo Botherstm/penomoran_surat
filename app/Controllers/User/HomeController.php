edfd065c6f5532e8d477a20f0f4652583e19f9b6
Botherstm <jamesloro12012002@gmail.com>
2023-10-12 11:53:57 +0800

    login

--- app/Controllers/User/HomeController.php
+++ app/Controllers/User/HomeController.php
@@ -8,6 +8,12 @@ class HomeController extends BaseController
 {
     public function index()
     {
+        session();
+        if (!session()->has('user_id')) {
+            return view('login', [
+                'validation' => \Config\Services::validation()
+            ]);
+        }
           return view('public/index');
     }
 
