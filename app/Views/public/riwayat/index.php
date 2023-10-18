1128ecb5d84814941cc420e26aa2499c1cc27616
aryayasodaputra <petrusganteng868@gmail.com>
2023-10-18 08:42:04 +0700

    perubahan

--- app/Views/public/riwayat/index.php
+++ app/Views/public/riwayat/index.php
@@ -11,8 +11,7 @@
                     <div class="col-md-8 offset-md-2">
                         <form action="simple-results.html">
                             <div class="input-group input-group-sm" style="width: 150px;">
-                                <input type="text" id="searchInput" class="form-control float-right"
-                                    placeholder="Search">
+                                <input type="text" id="searchInput" class="form-control float-right" placeholder="Search">
                                 <div class="input-group-append">
                                     <button type="button" id="searchButton" class="btn btn-default">
                                         <i class="fas fa-search"></i>
@@ -30,38 +29,36 @@
     <div class="container-fluid" style="padding-top: 50px; padding-bottom: 50px;">
         <section class="content">
             <div class="row">
-                <?php foreach ($generates as $key => $generate): ?>
-                <div class="col-lg-4 col-md-6 col-sm-12">
-                    <div class="card" style="margin-bottom: 20px;">
-                        <div class="card-body">
-                            <a href="<?php echo base_url('public/riwayat/detail/')?><?= $generate['slug']; ?>"
-                                class="text-dark text-decoration-none" style="text-decoration:none; ">
-                                <h5 class="card-title">
+                <?php foreach ($generates as $key => $generate) : ?>
+                    <div class="col-lg-4 col-md-6 col-sm-12">
+                        <div class="card" style="margin-bottom: 20px;">
+                            <div class="card-body">
+                                <a href="<?php echo base_url('public/riwayat/detail/') ?><?= $generate['slug']; ?>" class="text-dark text-decoration-none" style="text-decoration:none; ">
+                                    <h5 class="card-title">
 
-                                    <b class="">Perihal Surat:</b>
+                                        <b class="">Perihal Surat:</b>
 
-                                    <?= $generate['perihal']; ?>
+                                        <?= $generate['perihal']; ?>
 
-                                </h5>
-                                <br>
-                                <h5 class="card-title"><b>Kode Surat :</b> <?= $generate['nomor']; ?></h5>
-                                <br>
-                                <h5 class="card-title"><b>Tanggal Surat :</b>
-                                    <?= date('d F Y', strtotime($generate['tanggal'])); ?></h5>
-                                <br>
-                                <p class="card-text"><b>User yang menggenerate : </b><?= $user['name']; ?></p>
-                                <br>
-                            </a>
-                            <a target="_blank" href="<?= base_url('pdf/'); ?><?= $generate['pdf']; ?>">
-                                <button class="btn btn-dark">Download PDF</button>
-                            </a>
-                            <p class="card-text"><small
-                                    class="text-muted"><?= (new \CodeIgniter\I18n\Time($generate['created_at']))->humanize(); ?></small>
-                            </p>
+                                    </h5>
+                                    <br>
+                                    <h5 class="card-title"><b>Kode Surat :</b> <?= $generate['nomor']; ?></h5>
+                                    <br>
+                                    <h5 class="card-title"><b>Tanggal Surat :</b>
+                                        <?= date('d F Y', strtotime($generate['tanggal'])); ?></h5>
+                                    <br>
+                                    <p class="card-text"><b>User yang menggenerate : </b><?= $user['name']; ?></p>
+                                    <br>
+                                </a>
+                                <a target="_blank" href="<?= base_url('pdf/'); ?><?= $generate['pdf']; ?>">
+                                    <button class="p-3 text-primary-emphasis bg-success-subtle border border-primary-subtle rounded-3">Download PDF</button>
+                                </a>
+                                <p class="card-text"><small class="text-muted"><?= (new \CodeIgniter\I18n\Time($generate['created_at']))->humanize(); ?></small>
+                                </p>
 
+                            </div>
                         </div>
                     </div>
-                </div>
                 <?php endforeach ?>
             </div>
         </section>
@@ -71,35 +68,35 @@
 
 
 <script>
-<?php if (session()->getFlashdata('success')) : ?>
-Swal.fire({
-    title: 'Success',
-    text: '<?= session()->getFlashdata('success') ?>',
-    icon: 'success',
-    timer: 3000,
-    showConfirmButton: false
-});
-<?php endif; ?>
+    <?php if (session()->getFlashdata('success')) : ?>
+        Swal.fire({
+            title: 'Success',
+            text: '<?= session()->getFlashdata('success') ?>',
+            icon: 'success',
+            timer: 3000,
+            showConfirmButton: false
+        });
+    <?php endif; ?>
 
-function performSearch() {
-    var searchText = document.getElementById('searchInput').value.toLowerCase();
-    var cardDeck = document.getElementById('cardDeck');
-    var cards = cardDeck.getElementsByClassName('card');
+    function performSearch() {
+        var searchText = document.getElementById('searchInput').value.toLowerCase();
+        var cardDeck = document.getElementById('cardDeck');
+        var cards = cardDeck.getElementsByClassName('card');
 
-    for (var i = 0; i < cards.length; i++) {
-        var card = cards[i];
-        var cardText = card.innerText.toLowerCase();
-        if (cardText.includes(searchText)) {
-            card.style.display = 'block';
-        } else {
-            card.style.display = 'none';
+        for (var i = 0; i < cards.length; i++) {
+            var card = cards[i];
+            var cardText = card.innerText.toLowerCase();
+            if (cardText.includes(searchText)) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
         }
     }
-}
 
-document.getElementById('searchButton').addEventListener('click', performSearch);
+    document.getElementById('searchButton').addEventListener('click', performSearch);
 
-document.getElementById('searchInput').addEventListener('input', performSearch);
+    document.getElementById('searchInput').addEventListener('input', performSearch);
 </script>
 
 <?= $this->endSection('content'); ?>
