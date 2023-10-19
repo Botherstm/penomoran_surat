d46c09accf330fb216389645efd15036df0a8d85
aryayasodaputra <petrusganteng868@gmail.com>
2023-10-19 09:02:53 +0700

    perubahan

--- app/Views/public/layouts/sidebar.php
+++ app/Views/public/layouts/sidebar.php
@@ -73,7 +73,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
+                        <a href="/user/profile" class="nav-link">
                             <i class="nav-icon fa fa-user"></i>
                             <p>
                                 Pengaturan Akun
