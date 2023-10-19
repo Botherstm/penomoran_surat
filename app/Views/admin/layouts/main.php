8c39e65495068b16a1541d799f01395f72383604
Botherstm <jamesloro12012002@gmail.com>
2023-10-19 11:35:40 +0800

    perubahan

--- app/Views/admin/layouts/main.php
+++ app/Views/admin/layouts/main.php
@@ -12,26 +12,26 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.3/dist/sweetalert2.min.css">
 
-    <link rel="stylesheet" href="<?php echo base_url()?>plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
     <link rel="stylesheet"
-        href="<?php echo base_url()?>adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+        href="<?php echo base_url() ?>adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
     <!-- iCheck -->
-    <link rel="stylesheet" href="<?php echo base_url()?>adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- JQVMap -->
-    <link rel="stylesheet" href="<?php echo base_url()?>adminlte/plugins/jqvmap/jqvmap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>adminlte/plugins/jqvmap/jqvmap.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="<?php echo base_url()?>adminlte/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>adminlte/dist/css/adminlte.min.css">
     <!-- overlayScrollbars -->
     <link rel="stylesheet"
-        href="<?php echo base_url()?>adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+        href="<?php echo base_url() ?>adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="<?php echo base_url()?>adminlte/plugins/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>adminlte/plugins/daterangepicker/daterangepicker.css">
     <!-- summernote -->
-    <link rel="stylesheet" href="<?php echo base_url()?>adminlte/plugins/summernote/summernote-bs4.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>adminlte/plugins/summernote/summernote-bs4.min.css">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="<?php echo base_url() ?>adminlte/plugins/fontawesome-free/css/all.min.css">
@@ -115,47 +115,47 @@
 
 <body class="hold-transition sidebar-mini layout-navbar-fixed">
     <div class="wrapper">
-        <?= $this->include('admin/layouts/navbar'); ?>
-        <?= $this->include('admin/layouts/sidebar'); ?>
+        <?=$this->include('admin/layouts/navbar');?>
+        <?=$this->include('admin/layouts/sidebar');?>
         <!-- Main content -->
         <section class="content">
-            <?= $this->renderSection('content'); ?>
+            <?=$this->renderSection('content');?>
         </section>
-        <?= $this->include('admin/layouts/footer'); ?>
+        <?=$this->include('admin/layouts/footer');?>
     </div>
     <!-- Control Sidebar -->
 
     <!-- /.control-sidebar -->
     </div>
     <!-- ./wrapper -->
-    <script src="<?php echo base_url()?>adminlte/plugins/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url() ?>adminlte/plugins/jquery/jquery.min.js"></script>
     <!-- jQuery UI 1.11.4 -->
-    <script src="<?php echo base_url()?>adminlte/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <script src="<?php echo base_url() ?>adminlte/plugins/jquery-ui/jquery-ui.min.js"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
     $.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 4 -->
-    <script src="<?php echo base_url()?>adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url() ?>adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- ChartJS -->
-    <script src="<?php echo base_url()?>adminlte/plugins/chart.js/Chart.min.js"></script>
+    <script src="<?php echo base_url() ?>adminlte/plugins/chart.js/Chart.min.js"></script>
     <!-- Sparkline -->
-    <script src="<?php echo base_url()?>adminlte/plugins/sparklines/sparkline.js"></script>
+    <script src="<?php echo base_url() ?>adminlte/plugins/sparklines/sparkline.js"></script>
     <!-- JQVMap -->
-    <script src="<?php echo base_url()?>adminlte/plugins/jqvmap/jquery.vmap.min.js"></script>
-    <script src="<?php echo base_url()?>adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+    <script src="<?php echo base_url() ?>adminlte/plugins/jqvmap/jquery.vmap.min.js"></script>
+    <script src="<?php echo base_url() ?>adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
     <!-- jQuery Knob Chart -->
-    <script src="<?php echo base_url()?>adminlte/plugins/jquery-knob/jquery.knob.min.js"></script>
+    <script src="<?php echo base_url() ?>adminlte/plugins/jquery-knob/jquery.knob.min.js"></script>
     <!-- daterangepicker -->
-    <script src="<?php echo base_url()?>adminlte/plugins/moment/moment.min.js"></script>
-    <script src="<?php echo base_url()?>adminlte/plugins/daterangepicker/daterangepicker.js"></script>
+    <script src="<?php echo base_url() ?>adminlte/plugins/moment/moment.min.js"></script>
+    <script src="<?php echo base_url() ?>adminlte/plugins/daterangepicker/daterangepicker.js"></script>
     <!-- Tempusdominus Bootstrap 4 -->
-    <script src="<?php echo base_url()?>adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js">
+    <script src="<?php echo base_url() ?>adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js">
     </script>
     <!-- Summernote -->
-    <script src="<?php echo base_url()?>adminlte/plugins/summernote/summernote-bs4.min.js"></script>
+    <script src="<?php echo base_url() ?>adminlte/plugins/summernote/summernote-bs4.min.js"></script>
     <!-- overlayScrollbars -->
-    <script src="<?php echo base_url()?>adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <script src="<?php echo base_url() ?>adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 
 
     <script src="<?php echo base_url() ?>adminlte/plugins/jquery/jquery.min.js"></script>
@@ -206,7 +206,6 @@
     <script src="<?php echo base_url() ?>adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 
     <script src="<?php echo base_url() ?>adminlte/dist/js/adminlte.min.js?v=3.2.0"></script>
-
     <script>
     $(function() {
         $("#example1").DataTable({
