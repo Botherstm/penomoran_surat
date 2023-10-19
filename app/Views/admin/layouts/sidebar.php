15e9342875a2bc162a6225d168acbc0f406cc261
Botherstm <jamesloro12012002@gmail.com>
2023-10-19 09:20:07 +0800

    perubahan

--- app/Views/admin/layouts/sidebar.php
+++ app/Views/admin/layouts/sidebar.php
@@ -1,19 +1,10 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> 72278674bc9222599d9e33c3ec349b0dae2f3a76
     <a href="index3.html" class="brand-link bg-teal ">
         <img src="https://i.ibb.co/wph2S6n/singa-ambara-raja.png" alt="singa-ambara-raja" class="justify-content-center"
             height="40" width="65">
         <span class="brand-text font-weight-bold ">E-NOMOR Admin</span>
     </a>
-<<<<<<< HEAD
-=======
-
->>>>>>> 72278674bc9222599d9e33c3ec349b0dae2f3a76
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
@@ -22,7 +13,7 @@
                 <img src="adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div> -->
             <div class="info">
-                <a href="#" class="d-block"><?= session()->get('name'); ?></a>
+                <a href="#" class="d-block"><?=session()->get('name');?></a>
             </div>
         </div>
 
@@ -67,7 +58,7 @@
                         </li>
                     </ul>
                 </li>
-                <?php if(session()->get('level') == 1): ?>
+                <?php if (session()->get('level') == 1): ?>
 
                 <li class="nav-item ">
                     <a href="<?php echo base_url('admin/bidang/') ?>" class="nav-link">
@@ -85,7 +76,7 @@
                         </p>
                     </a>
                 </li>
-                <?php elseif(session()->get('level') == 2): ?>
+                <?php elseif (session()->get('level') == 2): ?>
                 <li class="nav-item ">
                     <a href="<?php echo base_url('admin/dinas/') ?>" class="nav-link">
                         <i class="nav-icon fas fa-city"></i>
@@ -120,7 +111,7 @@
                         </li>
                     </ul>
                 </li>
-                <?php endif; ?>
+                <?php endif;?>
                 <li class="nav-item ">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-info"></i>
