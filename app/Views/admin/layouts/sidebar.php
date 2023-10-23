1f2261aaed191bd996461853d4e9e733a174f602
YanDjavier <yonimheswara@gmail.com>
2023-10-23 11:42:36 +0800

    revisiwakwaw22

--- app/Views/admin/layouts/sidebar.php
+++ app/Views/admin/layouts/sidebar.php
@@ -1,17 +1,18 @@
-<aside class="main-sidebar sidebar-light-primary elevation-4" style="position: fixed;">
+<aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link bg-teal ">
+
+    <!-- ready -->
+
+  <a href="index3.html" class="brand-link bg-teal ">
         <img src="https://i.ibb.co/wph2S6n/singa-ambara-raja.png" alt="singa-ambara-raja" class="justify-content-center"
             height="40" width="65">
-        <span class="brand-text font-weight-bold ">E-NOMOR Admin</span>
+        <span class="brand-text font-weight-bold " style="text-shadow: 2px 2px 1px grey; padding-top: 10px;">E-NOMOR Admin</span>
     </a>
-    <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <?php if ((session()->get('gambar') !== null)): ?>
             <div class="image">
-                <img src="<?php echo base_url('userimage/') ?><?=session()->get('gambar');?>" width="20px"
+                <img src="<?php echo base_url('userimage/') ?><?=session()->get('gambar');?>"
                     class="img-circle elevation-2" alt="User Image">
             </div>
             <?php else: ?>
@@ -24,10 +25,9 @@
 
         <!-- SidebarSearch Form -->
 
-
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                 <li class="nav-item ">
@@ -74,29 +74,13 @@
                 <!-- Develop KATEGORI, perihal, sub perihal, detail  -->
                 <li class="nav-item ">
                     <a href="<?php echo base_url('admin/kategori') ?>" class="nav-link">
-                        <i class="nav-icon fas fa-book"></i>
+                    <i class="nav-icon fas fa-book"></i>
                         <p>
-                            Kategori
-                            <i class="fas fa-angle-left right"></i>
+                        Kategori
                         </p>
                     </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="<?php echo base_url('admin/kategori') ?>" class="nav-link">
-                                <i class="nav-icon fas fa-pen-nib"></i>
-                                <p>List Kategori</p>
-                            </a>
-                        </li>
-                    </ul>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="<?php echo base_url('admin/kategori/create') ?>" class="nav-link">
-                                <i class="nav-icon fas fa-pen-nib"></i>
-                                <p>Tambah Kategori</p>
-                            </a>
-                        </li>
-                    </ul>
                 </li>
+              
                 <?php endif;?>
                 <li class="nav-item ">
                     <a href="#" class="nav-link">
@@ -114,18 +98,18 @@
                         </p>
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item"  >
                     <a href="/logout" class="nav-link">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi bi-box-arrow-left m-1 " viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="auto" fill="currentColor"
+                            class="bi bi-box-arrow-left  " viewBox="0 0 16 16">
                             <path fill-rule="evenodd"
                                 d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z" />
                             <path fill-rule="evenodd"
                                 d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
                         </svg>
-                        <p class="m-1">
+                        <po class="m-1" >
                             Keluar
-                        </p>
+                        </po>
                     </a>
                 </li>
             </ul>
@@ -133,4 +117,7 @@
         <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
+
+
+
 </aside>
