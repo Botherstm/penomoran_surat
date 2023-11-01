4b0918ba933d59027c958d0d179cec8b3fbfca7d
Botherstm <jamesloro12012002@gmail.com>
2023-11-01 13:58:22 +0800

    perubahan

--- app/Views/login.php
+++ app/Views/login.php
@@ -8,7 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | E Nomor Pemkab Buleleng</title>
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.6/dist/sweetalert2.min.css">
@@ -17,9 +18,9 @@
     </script>
 
     <style>
-        .navbar {
-            background-color: rgba(32, 201, 151, 255);
-        }
+    .navbar {
+        background-color: rgba(32, 201, 151, 255);
+    }
     </style>
 </head>
 
@@ -28,8 +29,10 @@
     <nav class="navbar navbar-expand-lg navbar-dark shadow position-relative">
         <div class="container">
             <a class="navbar-brand d-flex" href="#">
-                <img src="<?php echo base_url('img/logo-kabupaten-buleleng.png') ?>" alt="Pemkab Buleleng" style="width: 70px;">
-                <div class="ms-1 d-flex flex-column align-items-start" style="padding-left: 15px; padding-top: 5px; text-shadow: 2px 2px 1px grey; font-weight: bold; ">
+                <img src="<?php echo base_url('img/logo-kabupaten-buleleng.png') ?>" alt="Pemkab Buleleng"
+                    style="width: 70px;">
+                <div class="ms-1 d-flex flex-column align-items-start"
+                    style="padding-left: 15px; padding-top: 5px; text-shadow: 2px 2px 1px grey; font-weight: bold; ">
                     <span>E-NOMOR</span>
                     <span class="small">KOMINFO SANTI</span>
                 </div>
@@ -54,22 +57,27 @@
                             </div>
                             <div class="mb-3">
                                 <div class="input-group">
-                                    <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" required placeholder="Email" style="opacity: 0.7;">
+                                    <input type="email" class="form-control" name="email" id="exampleInputEmail1"
+                                        aria-describedby="emailHelp" required placeholder="Email" style="opacity: 0.7;">
                                     <i class="input-group-text bi bi-person-fill"></i>
                                 </div>
                             </div>
                             <div class="mb-1" style="padding-bottom: 5%;">
                                 <div class="input-group">
-                                    <input type="password" class="form-control <?=($validation->hasError('password')) ? 'is-invalid' : '';?>" required id="exampleInputPassword1" placeholder="Password" name="password" style="opacity: 0.7;">
+                                    <input type="password"
+                                        class="form-control <?=($validation->hasError('password')) ? 'is-invalid' : '';?>"
+                                        required id="exampleInputPassword1" placeholder="Password" name="password"
+                                        style="opacity: 0.7;">
                                     <?php if ($validation->hasError('password')): ?>
-                                        <div class="invalid-feedback">
-                                            <?=$validation->getError('password');?>
-                                        </div>
+                                    <div class="invalid-feedback">
+                                        <?=$validation->getError('password');?>
+                                    </div>
                                     <?php endif;?>
                                     <span class="input-group-text bi bi-eye-slash" id="showPassword"></span>
                                 </div>
                             </div>
-                            <div class="g-recaptcha " data-sitekey="<?=$key;?>" style="width: 100%; max-width: 300px; margin: 0 auto;"></div>
+                            <div class="g-recaptcha " data-sitekey="<?=$key;?>"
+                                style="width: 100%; max-width: 300px; margin: 0 auto;"></div>
                             <br>
                             <div class="d-grid gap-2 col-6 mx-auto mb-1">
                                 <button class="btn btn-success text-light" type="submit">Login</button>
@@ -88,60 +96,60 @@
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.3/dist/sweetalert2.all.min.js"></script>
     <?php if (session()->getFlashdata('success')): ?>
-        <script>
-            Swal.fire({
-                icon: 'success',
-                title: 'Berhasil',
-                text: '<?php echo session()->getFlashdata('success'); ?>',
-            });
-        </script>
+    <script>
+    Swal.fire({
+        icon: 'success',
+        title: 'Berhasil',
+        text: '<?php echo session()->getFlashdata('success'); ?>',
+    });
+    </script>
     <?php endif;?>
     <?php if (session()->getFlashdata('error')): ?>
-        <script>
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: '<?php echo session()->getFlashdata('error'); ?>',
-            });
-        </script>
+    <script>
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: '<?php echo session()->getFlashdata('error'); ?>',
+    });
+    </script>
     <?php endif;?>
     <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            const form = document.querySelector("form");
-            const submitButton = form.querySelector("button");
-            const recaptcha = document.querySelector(".g-recaptcha");
+    document.addEventListener("DOMContentLoaded", function() {
+        const form = document.querySelector("form");
+        const submitButton = form.querySelector("button");
+        const recaptcha = document.querySelector(".g-recaptcha");
 
-            // Nonaktifkan tombol "Login" saat halaman dimuat
-            submitButton.disabled = false;
+        // Nonaktifkan tombol "Login" saat halaman dimuat
+        submitButton.disabled = false;
 
-            recaptcha.addEventListener("change", function() {
-                const recaptchaResponse = grecaptcha.getResponse();
-                // Aktifkan tombol "Login" jika reCAPTCHA diverifikasi
-                if (recaptchaResponse.length > 0) {
-                    submitButton.disabled = false;
-                }
-            });
+        recaptcha.addEventListener("change", function() {
+            const recaptchaResponse = grecaptcha.getResponse();
+            // Aktifkan tombol "Login" jika reCAPTCHA diverifikasi
+            if (recaptchaResponse.length > 0) {
+                submitButton.disabled = false;
+            }
         });
+    });
     </script>
 
     <script>
-        function showAlert() {
-            Swal.fire('berhasil');
-        }
-        const passwordInput = document.getElementById("exampleInputPassword1");
-        const showPasswordIcon = document.getElementById("showPassword");
+    function showAlert() {
+        Swal.fire('berhasil');
+    }
+    const passwordInput = document.getElementById("exampleInputPassword1");
+    const showPasswordIcon = document.getElementById("showPassword");
 
-        showPasswordIcon.addEventListener("click", function() {
-            if (passwordInput.type === "password") {
-                passwordInput.type = "text";
-                showPasswordIcon.classList.remove("bi-eye-slash");
-                showPasswordIcon.classList.add("bi-eye");
-            } else {
-                passwordInput.type = "password";
-                showPasswordIcon.classList.remove("bi-eye");
-                showPasswordIcon.classList.add("bi-eye-slash");
-            }
-        });
+    showPasswordIcon.addEventListener("click", function() {
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            showPasswordIcon.classList.remove("bi-eye-slash");
+            showPasswordIcon.classList.add("bi-eye");
+        } else {
+            passwordInput.type = "password";
+            showPasswordIcon.classList.remove("bi-eye");
+            showPasswordIcon.classList.add("bi-eye-slash");
+        }
+    });
     </script>
 </body>
 
