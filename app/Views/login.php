b674e4f5dc3cf5f2c338a162d46d83407691075f
YanDjavier <yonimheswara@gmail.com>
2023-11-02 15:19:14 +0800

    revisiwakwaw74

--- app/Views/login.php
+++ app/Views/login.php
@@ -33,23 +33,23 @@
 
                     <!-- Logo Aplikasi -->
                     <div class="col-md-5">
-                        <div class="card shadow-lg bg-success bg-gradient text-white " style="height: 100%; --bs-bg-opacity: .9;">
+                        <div class="card shadow-lg bg-success bg-gradient text-white " style="height: 95%; --bs-bg-opacity: .9;">
                             <div class="card-body ">
                                 <div class=" text-center">
 
                                     <img src="<?php echo base_url('img/logo-kabupaten-buleleng.png') ?>" alt="Pemkab Buleleng" style="max-width: 20%; ">
-                                    <img src="/img/logo_kominfosanti_buleleng.png" alt="" style="max-width: 25%; ">
+                                    <img src="/img/logo_kominfosanti_buleleng.png" alt="" style="max-width: 20%; ">
 
                                 </div>
                                 <hr>
                                 <div class=" text-center ">
-                                    <h6><small>VISI PEMBANGUNAN KABUPATEN BULELENG</small></h6>
-                                    <p class="fs-7"><small>“TERWUJUDNYA MASYARAKAT BULELENG YANG MANDIRI SEJAHTERA DAN BERDAYA SAING BERLANDASKAN TRI HITA KARANA”</small></p>
+                                    <h6 style=" font-size: small; " ><small>VISI PEMBANGUNAN KABUPATEN BULELENG</small></h6>
+                                    <p  style=" font-size: small; " ><small>“TERWUJUDNYA MASYARAKAT BULELENG YANG MANDIRI SEJAHTERA DAN BERDAYA SAING BERLANDASKAN TRI HITA KARANA”</small></p>
                                 </div>
                                 <div>
-                                    <h6 class=" text-center "><small>MISI PEMBANGUNAN KABUPATEN BULELENG</small></h6>
+                                    <h6 class=" text-center " style=" font-size: small; "><small>MISI PEMBANGUNAN KABUPATEN BULELENG</small></h6>
                                     <small>
-                                        <ol>
+                                        <ol style="font-size: smaller;" >
                                             <li>Memantapkan Pembangunan Ekonomi untuk Mewujudkan Pertumbuhan Ekonomi yang Inklusif;</li>
                                             <li>Pengembangan Ekonomi Kerakyatan yang Berbasis pada Produk Unggulan Daerah;</li>
                                             <li>Meningkatkan Kualitas Sumber Daya Manusia yang Profesional, Berbudaya dan Berintegritas; </li>
@@ -67,18 +67,18 @@
 
                     <!-- Form Login -->
                     <div class="col-md-4 mt-2">
-                        <div class="card shadow-lg p-3 bg-dark bg-gradient" style="height: 100%; --bs-bg-opacity: .1;">
+                        <div class="card shadow-lg p-3 bg-dark bg-gradient" style="height: 95%; --bs-bg-opacity: .1;">
                             <div class="card-body">
                                 <form class="form" method="POST" action="<?php echo base_url('login') ?>">
                                     <?= csrf_field(); ?>
-                                    <div class="text-center mb-4">
-                                        <div class="ms-1 d-flex flex-column" style=" font-weight: bold; ">
-                                            <span class="fs-4">E-NOMOR</span>
-                                            <span class="fs-4">KOMINFO SANTI</span>
+                                    <div class="text-center ">
+                                        <div class=" d-flex flex-column" style=" font-weight: bold; ">
+                                            <span class="fs-6">E-NOMOR</span>
+                                            <span class="fs-7">KOMINFO SANTI</span>
                                         </div>
                                     </div>
                                     <hr>
-                                    <div class="input-group justify-content-center my-4 mt-5">
+                                    <div class="input-group justify-content-center mb-2 ">
                                         <h2>LOGIN</h2>
                                     </div>
                                     <div class="mb-3 ">
@@ -87,7 +87,7 @@
                                             <i class="shadow input-group-text bi bi-person-fill"></i>
                                         </div>
                                     </div>
-                                    <div class="mb-2" style="padding-bottom: 10%;">
+                                    <div class="mb-2" style="padding-bottom: 6%;">
                                         <div class=" input-group ">
                                             <input type="password" class="shadow form-control <?= ($validation->hasError('password')) ? 'is-invalid' : ''; ?>" required id="exampleInputPassword1" placeholder="Password" name="password" style="opacity: 0.7;">
                                             <?php if ($validation->hasError('password')) : ?>
@@ -98,7 +98,7 @@
                                             <span class="shadow input-group-text bi bi-eye-slash" id="showPassword"></span>
                                         </div>
                                     </div>
-                                    <div class="g-recaptcha " data-sitekey="<?= $key; ?>" style="width: 100%; max-width: 300px; margin: 0 auto;"></div>
+                                    <div class="g-recaptcha " data-sitekey="<?= $key; ?>" style="width: 100%; max-width: auto; margin: 0 auto;"></div>
                                     <br>
                                     <div class="d-grid gap-2 col-6 mx-auto mb-3">
                                         <button class="shadow text-light btn btn-success" type="submit">Login</button>
