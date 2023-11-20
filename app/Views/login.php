8037ce7e28dc9580ef72dc089d67306f6525e817
aryayasodaputra <petrusganteng868@gmail.com>
2023-11-20 20:21:19 +0700

    perubahan

--- app/Views/login.php
+++ app/Views/login.php
@@ -6,8 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | E Nomor Pemkab Buleleng</title>
     <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.6/dist/sweetalert2.min.css">
@@ -23,34 +22,30 @@
         <!-- Content Header (Page header) -->
         <div class="content-header">
             <div class="container-fluid">
-                <div class="row d-flex justify-content-center w-auto p-3 "
-                    style="margin: auto; height:5%; margin-top: 1%;">
+                <div class="row d-flex justify-content-center w-auto p-3 " style="margin: auto; height:5%; margin-top: 1%;">
 
-                    <?php if (session('errors')): ?>
-                    <div class="alert alert-danger">
-                        <ul>
-                            <li><?=esc(session('errors'))?></li>
-                        </ul>
-                        <button id="dismissError" class="btn btn-dark" style="width: 10%;">Hide</button>
-                    </div>
-                    <?php endif;?>
-
-                    <?php if ($cooldownTime > time()): ?>
-                        <div id="cooldown-info" class="alert alert-warning" role="alert" >
-                            Anda telah mencoba login beberapa kali. Silakan coba lagi dalam <span
-                                id="cooldown-counter"></span> detik.
+                    <?php if (session('errors')) : ?>
+                        <div class="alert alert-danger">
+                            <ul>
+                                <li><?= esc(session('errors')) ?></li>
+                            </ul>
+                            <button id="dismissError" class="btn btn-dark" style="width: 10%;">Hide</button>
                         </div>
-                        <?php endif; ?>
+                    <?php endif; ?>
+
+                    <?php if ($cooldownTime > time()) : ?>
+                        <div id="cooldown-info" class="alert alert-warning" role="alert">
+                            Anda telah mencoba login beberapa kali. Silakan coba lagi dalam <span id="cooldown-counter"></span> detik.
+                        </div>
+                    <?php endif; ?>
 
                     <!-- Logo Aplikasi -->
                     <div class="col-md-5">
-                        <div class="card shadow-lg bg-success bg-gradient text-white "
-                            style="height: 95%; --bs-bg-opacity: .9;">
+                        <div class="card shadow-lg bg-success bg-gradient text-white " style="height: 95%; --bs-bg-opacity: .9;">
                             <div class="card-body ">
                                 <div class=" text-center">
 
-                                    <img src="<?= base_url('img/logo-kabupaten-buleleng.png') ?>" alt="Pemkab Buleleng"
-                                        style="max-width: 20%; ">
+                                    <img src="<?= base_url('img/logo-kabupaten-buleleng.png') ?>" alt="Pemkab Buleleng" style="max-width: 20%; ">
                                     <img src="/img/logo_kominfosanti_buleleng.png" alt="" style="max-width: 25%; ">
 
                                 </div>
@@ -85,14 +80,14 @@
                         </div>
                     </div>
 
- 
+
                     <!-- Form Login -->
                     <div class="col-md-4 mt-2">
-                        
+
                         <div class="card shadow-lg p-3 bg-dark bg-gradient" style="height: 95%; --bs-bg-opacity: .1;">
                             <div class="card-body">
                                 <form class="form" method="POST" action="<?php echo base_url('login') ?>">
-                                    <?=csrf_field();?>
+                                    <?= csrf_field(); ?>
                                     <div class="text-center ">
                                         <div class=" d-flex flex-column" style=" font-weight: bold; ">
                                             <span class="fs-6">E-NOMOR</span>
@@ -105,24 +100,18 @@
                                     </div>
                                     <div class="mb-3 ">
                                         <div class="input-group">
-                                            <input type="name" autofocus class="shadow form-control" name="username"
-                                                id="exampleInputEmail1" aria-describedby="emailHelp" required
-                                                placeholder="Email" style="opacity: 0.7;">
+                                            <input type="name" autofocus class="shadow form-control" name="username" id="exampleInputEmail1" aria-describedby="emailHelp" required placeholder="User Name" style="opacity: 0.7;">
                                             <i class="shadow input-group-text bi bi-person-fill"></i>
                                         </div>
                                     </div>
                                     <div class="mb-2" style="padding-bottom: 6%;">
                                         <div class=" input-group ">
-                                            <input type="password" class="shadow form-control "
-                                                id="exampleInputPassword1" placeholder="Password" name="password"
-                                                style="opacity: 0.7;">
+                                            <input type="password" class="shadow form-control " id="exampleInputPassword1" placeholder="Password" name="password" style="opacity: 0.7;">
 
-                                            <span class="shadow input-group-text bi bi-eye-slash"
-                                                id="showPassword"></span>
+                                            <span class="shadow input-group-text bi bi-eye-slash" id="showPassword"></span>
                                         </div>
                                     </div>
-                                    <div class="g-recaptcha " data-sitekey="<?=$key;?>"
-                                        style="width: 100%; max-width: auto; margin: 0 auto;"></div>
+                                    <div class="g-recaptcha " data-sitekey="<?= $key; ?>" style="width: 100%; max-width: auto; margin: 0 auto;"></div>
                                     <br>
                                     <div class="d-grid gap-2 col-6 mx-auto mb-3">
                                         <button class="shadow text-light btn btn-success" type="submit">Login</button>
@@ -138,70 +127,70 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.3/dist/sweetalert2.all.min.js"></script>
-    <?php if (session()->getFlashdata('success')): ?>
-    <script>
-    Swal.fire({
-        icon: 'success',
-        title: 'Berhasil',
-        text: '<?php echo session()->getFlashdata('success'); ?>',
-    });
-    </script>
-    <?php endif;?>
+    <?php if (session()->getFlashdata('success')) : ?>
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Berhasil',
+                text: '<?php echo session()->getFlashdata('success'); ?>',
+            });
+        </script>
+    <?php endif; ?>
 
 
     <script>
-    function showAlert() {
-        Swal.fire('berhasil');
-    }
-    const passwordInput = document.getElementById("exampleInputPassword1");
-    const showPasswordIcon = document.getElementById("showPassword");
-
-    showPasswordIcon.addEventListener("click", function() {
-        if (passwordInput.type === "password") {
-            passwordInput.type = "text";
-            showPasswordIcon.classList.remove("bi-eye-slash");
-            showPasswordIcon.classList.add("bi-eye");
-        } else {
-            passwordInput.type = "password";
-            showPasswordIcon.classList.remove("bi-eye");
-            showPasswordIcon.classList.add("bi-eye-slash");
+        function showAlert() {
+            Swal.fire('berhasil');
         }
-    });
-    </script>
-    <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        const dismissButton = document.getElementById("dismissError");
-        const errorAlert = document.querySelector(".alert.alert-danger");
+        const passwordInput = document.getElementById("exampleInputPassword1");
+        const showPasswordIcon = document.getElementById("showPassword");
 
-        dismissButton.addEventListener("click", function() {
-            errorAlert.style.display =
-                "none"; // Menyembunyikan pesan kesalahan saat tombol "Ok" ditekan
-        });
-    });
-    </script>
-    <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        var cooldownTime = <?= $cooldownTime ?>;
-        var cooldownCounterElement = document.getElementById("cooldown-counter");
-        var cooldownInfoElement = document.getElementById("cooldown-info");
-
-        function updateCooldownCounter() {
-            var currentTime = Math.floor(Date.now() / 1000);
-            var remainingTime = cooldownTime - currentTime;
-
-            if (remainingTime > 0) {
-                cooldownCounterElement.innerText = remainingTime;
+        showPasswordIcon.addEventListener("click", function() {
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                showPasswordIcon.classList.remove("bi-eye-slash");
+                showPasswordIcon.classList.add("bi-eye");
             } else {
-                cooldownInfoElement.style.display = "none";
+                passwordInput.type = "password";
+                showPasswordIcon.classList.remove("bi-eye");
+                showPasswordIcon.classList.add("bi-eye-slash");
             }
-        }
+        });
+    </script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const dismissButton = document.getElementById("dismissError");
+            const errorAlert = document.querySelector(".alert.alert-danger");
 
-        // Update counter every second
-        setInterval(updateCooldownCounter, 1000);
+            dismissButton.addEventListener("click", function() {
+                errorAlert.style.display =
+                    "none"; // Menyembunyikan pesan kesalahan saat tombol "Ok" ditekan
+            });
+        });
+    </script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var cooldownTime = <?= $cooldownTime ?>;
+            var cooldownCounterElement = document.getElementById("cooldown-counter");
+            var cooldownInfoElement = document.getElementById("cooldown-info");
 
-        // Initial update
-        updateCooldownCounter();
-    });
+            function updateCooldownCounter() {
+                var currentTime = Math.floor(Date.now() / 1000);
+                var remainingTime = cooldownTime - currentTime;
+
+                if (remainingTime > 0) {
+                    cooldownCounterElement.innerText = remainingTime;
+                } else {
+                    cooldownInfoElement.style.display = "none";
+                }
+            }
+
+            // Update counter every second
+            setInterval(updateCooldownCounter, 1000);
+
+            // Initial update
+            updateCooldownCounter();
+        });
     </script>
 
 </body>
